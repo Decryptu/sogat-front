3fadcb24c1a117a9452e71a2e1b31a8a49c206df
Decrypt <louis.vicomte@protonmail.com>
2024-11-14 14:08:46 +0100

    Revert "fix alias path"
    This reverts commit fbde67d0bc23d50fb20261486e6d759d22beb6af.

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,5 +1,5 @@
 import { getTranslations, setRequestLocale } from 'next-intl/server';
-import { SUPPORTED_LOCALES } from '../../i18n/routing';
+import { SUPPORTED_LOCALES } from '@/i18n/routing';
 
 export function generateStaticParams() {
   return SUPPORTED_LOCALES.map((locale) => ({ locale }));

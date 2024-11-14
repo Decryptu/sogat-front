919bdcf67d504ef5294d608db2e33b4215416b11
Decrypt <louis.vicomte@protonmail.com>
2024-11-14 14:10:06 +0100

    simplify

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,9 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 const withNextIntl = createNextIntlPlugin();
 
-// Get the directory name of the current module
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './src'),
-    };
-    return config;
-  },
   images: {
     remotePatterns: [
       {

4e3001e94d552492091fd42777928c12c5cbb7d4
Decrypt <louis.vicomte@protonmail.com>
2024-11-15 14:56:44 +0100

    use local font

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -1,11 +1,9 @@
-import { Geist } from 'next/font/google';
+import { geist, geistMono } from '../fonts'
 import { getMessages, setRequestLocale } from 'next-intl/server';
 import { NextIntlClientProvider } from 'next-intl';
 import { SUPPORTED_LOCALES } from '@/i18n/routing';
 import Header from '@/components/header/Header';
 import '@/app/globals.css';
-
-const geist = Geist({ subsets: ['latin'] });
 
 export function generateStaticParams() {
   return SUPPORTED_LOCALES.map((locale) => ({ locale }));
@@ -18,7 +16,7 @@ export default async function RootLayout({ children, params }) {
 
   return (
     <html lang={locale} className="h-full">
-      <body className={`${geist.className} h-full flex flex-col overflow-x-hidden`}>
+      <body className={`${geist.variable} ${geistMono.variable} font-sans h-full flex flex-col overflow-x-hidden`}>
         <NextIntlClientProvider messages={messages}>
           <Header />
           <main className="flex-1 relative">

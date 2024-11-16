d44eb508a375baa521836f45816fe4fe18cc779c
Decrypt <louis.vicomte@protonmail.com>
2024-11-16 12:05:02 +0100

    layout other pages

---
+++ src/app/[locale]/groupe/layout.jsx
@@ -0,0 +1,17 @@
+import { setRequestLocale } from 'next-intl/server';
+import { SUPPORTED_LOCALES } from '@/i18n/routing';
+
+export function generateStaticParams() {
+  return SUPPORTED_LOCALES.map((locale) => ({ locale }));
+}
+
+export default async function GroupeLayout({ children, params }) {
+  const { locale } = await params;
+  setRequestLocale(locale);
+
+  return (
+    <div className="w-full pt-20">
+      {children}
+    </div>
+  );
+}
